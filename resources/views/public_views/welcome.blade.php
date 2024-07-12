a531ca3fec73f99832082706a08613d4fadb8577
nualimah12 <alimahn123@gmail.com>
2024-07-12 07:27:06 +0700

    upload

--- resources/views/public_views/welcome.blade.php
+++ resources/views/public_views/welcome.blade.php
@@ -9,12 +9,12 @@
   <div class="row justify-content-center h-100">
     <div class="d-flex flex-column align-items-center main-content col-10 col-lg-8">
       <div class="logo-wrapper d-flex align-items-center justify-content-center gap-3">
-        <img src="{{asset('img/jti_logo.png')}}" class="jti-logo" />
-        <div class="text-wrapper gap-1">
-          <p class="m-0 head-title">Sistem Informasi</p>
-          <h1 class="fw-bold m-0 main-title">REPOSITORI SKRIPSI JTI</h1>
-        </div>
+        <img src="{{asset('img/logopal.png')}}" class="jti-logo" />
       </div>
+        <div class="text-wrapper gap-1">
+        {{-- <p class="m-0 head-title">Sistem Informasi</p> --}}
+          <h1 class="fw-bold m-0 main-title">Sistem Informasi Knowledge Sharing</h1>
+        </div>
       <div class="search-wrapper pb-2 mt-4 rounded rounded-3 w-100">
         <div class="input-group">
           <input type="text" class="form-control py-2 px-3 search-input border-0" placeholder="Search" name="title" />
